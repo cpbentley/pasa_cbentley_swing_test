4c79156d52c071ab9ecb1de3962bd2a4ff9a445c
Charles Bentley <cbentley@mail.ru>
2019-09-30 13:14:24 +0200

    swing ctx c5

--- src/pasa/cbentley/swing/utils/tests/TestSwingColorStore.java
+++ src/pasa/cbentley/swing/utils/tests/TestSwingColorStore.java
@@ -1,5 +1,6 @@
 package pasa.cbentley.swing.utils.tests;
 
+import pasa.cbentley.core.src5.ctx.C5Ctx;
 import pasa.cbentley.swing.ctx.SwingCtx;
 import pasa.cbentley.swing.utils.SwingColorStore;
 import pasa.cbentley.testing.BentleyTestCase;
@@ -11,7 +12,8 @@ public class TestSwingColorStore extends BentleyTestCase {
    protected SwingCtx sc;
    
    public void setupAbstract() {
-      sc = new SwingCtx(uc);
+      C5Ctx c5 = new C5Ctx(uc);
+      sc = new SwingCtx(c5);
       colorStore = new SwingColorStore(sc);
    }
 
